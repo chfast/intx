62765aba211dc202c3285415b3cd3e1d86218874
Paweł Bylica <chfast@gmail.com>
2019-01-17 15:18:04 +0100

    test: Extend decode.py script for fuzzing

--- test/fuzzer/decode.py
+++ test/fuzzer/decode.py
@@ -3,19 +3,25 @@
 import os
 import sys
 
-ops_filter = ('<<', '>>')
+ops_filter = ()
 
 ops = ('/', '*', '<<', '>>')
 
 
+def err(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
 def decode_file(file):
     with open(file, 'rb') as f:
+        print("Decoding {}".format(file))
         decode_data(f.read())
 
 
 def decode_data(data):
     arg_size = (len(data) - 1) // 2
     if arg_size not in (32, 64):
+        err("Incorrect argument size: {}".format(arg_size))
         return
 
     op_index = int(data[0])
@@ -29,13 +35,18 @@ def decode_data(data):
     x = int.from_bytes(data[1:1 + arg_size], byteorder='big')
     y = int.from_bytes(data[1 + arg_size:], byteorder='big')
 
+    print("argument size: {}".format(arg_size))
     print(x, op, y)
+    print(hex(x), op, hex(y))
 
 
 assert len(sys.argv) > 1
 
-corpus_dir = sys.argv[1]
+path = sys.argv[1]
 
-for root, _, files in os.walk(corpus_dir):
-    for file in files:
-        decode_file(os.path.join(root, file))
+if (os.path.isfile(path)):
+    decode_file(path)
+else:
+    for root, _, files in os.walk(path):
+        for file in files:
+            decode_file(os.path.join(root, file))
