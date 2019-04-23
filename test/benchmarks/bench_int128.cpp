e0687d2d30d4f18dbd242f61ef97eda7a6eb9ce1
Paweł Bylica <chfast@gmail.com>
2019-04-23 12:00:59 +0200

    test: Move umul() tests to int128 benchmarks

--- test/benchmarks/bench_int128.cpp
+++ test/benchmarks/bench_int128.cpp
@@ -5,6 +5,7 @@
 #include <intx/int128.hpp>
 
 #include "../utils/gmp.hpp"
+#include "../utils/random.hpp"
 #include <benchmark/benchmark.h>
 
 using namespace intx;
@@ -91,3 +92,27 @@ static void udiv128_single_long_shift(benchmark::State& state)
 BENCHMARK_TEMPLATE(udiv128_single_long_shift, div_gcc);
 BENCHMARK_TEMPLATE(udiv128_single_long_shift, div_uint128);
 BENCHMARK_TEMPLATE(udiv128_single_long_shift, div_gmp);
+
+
+template <typename RetT, RetT (*MulFn)(uint64_t, uint64_t)>
+static void umul128(benchmark::State& state)
+{
+    auto inputs = gen_uniform_seq(1000);
+    benchmark::ClobberMemory();
+
+    for (auto _ : state)
+    {
+        uint64_t alo = 0;
+        uint64_t ahi = 0;
+        for (size_t i = 0; i < inputs.size() - 1; ++i)
+        {
+            auto p = MulFn(inputs[i], inputs[i + 1]);
+            alo ^= p.lo;
+            ahi ^= p.hi;
+        }
+        benchmark::DoNotOptimize(alo);
+        benchmark::DoNotOptimize(ahi);
+    }
+}
+BENCHMARK_TEMPLATE(umul128, intx::uint128, intx::umul_generic);
+BENCHMARK_TEMPLATE(umul128, intx::uint128, intx::umul);
