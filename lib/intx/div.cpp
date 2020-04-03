f1a5309a27724f58965a5a7c953a45a1b126be1e
Paweł Bylica <chfast@gmail.com>
2020-04-03 19:10:08 +0200

    div: Optimize udivrem_by2() a bit
    Use iterator and do-while loop.

--- lib/intx/div.cpp
+++ lib/intx/div.cpp
@@ -67,13 +67,16 @@ inline uint128 udivrem_by2(uint64_t u[], int len, uint128 d) noexcept
 
     const auto reciprocal = reciprocal_3by2(d);
 
-    auto r = uint128{u[len - 1], u[len - 2]};  // Set the 2 top words as remainder.
+    auto rem = uint128{u[len - 1], u[len - 2]};  // Set the 2 top words as remainder.
     u[len - 1] = u[len - 2] = 0;  // Reset these words being a part of the result quotient.
 
-    for (int j = len - 3; j >= 0; --j)
-        std::tie(u[j], r) = udivrem_3by2(r.hi, r.lo, u[j], d, reciprocal);
+    auto it = &u[len - 3];
+    do
+    {
+        std::tie(*it, rem) = udivrem_3by2(rem.hi, rem.lo, *it, d, reciprocal);
+    } while (it-- != &u[0]);
 
-    return r;
+    return rem;
 }
 
 /// s = x + y.
