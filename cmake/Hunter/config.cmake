b5854211d655082114ecdee712234717977edcd7
Paweł Bylica <pawel@ethereum.org>
2024-01-04 13:15:34 +0100

    Add compatibility with CMake 3.27 and upgrade Hunter

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,8 +1,1 @@
 # Hunter local configuration.
-
-hunter_config(
-    benchmark
-    VERSION 1.6.1
-    URL https://github.com/google/benchmark/archive/v1.6.1.tar.gz
-    SHA1 1faaa54195824bbe151c1ebee31623232477d075
-)
