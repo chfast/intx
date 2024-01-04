803a28eddf6d0678846066a36d1487d104ba0714
Paweł Bylica <pawel@ethereum.org>
2024-01-04 15:12:58 +0100

    Add compatibility with CMake 3.27 and upgrade Hunter (#299)

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -2,27 +2,17 @@
 # Copyright 2019 Pawel Bylica.
 # Licensed under the Apache License, Version 2.0.
 
-# Figure out if Hunter is needed.
-if(INTX_TESTING OR INTX_BENCHMARKING)
-    set(hunter_enabled TRUE)
-else()
-    set(hunter_enabled FALSE)
-endif()
-
-option(HUNTER_ENABLED "Enable Hunter package manager" ${hunter_enabled})
-
 set(HUNTER_CONFIGURATION_TYPES Release CACHE STRING "Build type of the Hunter packages")
 set(HUNTER_USE_CACHE_SERVERS NO CACHE STRING "Use Hunter cache servers")
 
 file(
-    DOWNLOAD https://raw.githubusercontent.com/cpp-pm/gate/v0.9.2/cmake/HunterGate.cmake
+    DOWNLOAD https://raw.githubusercontent.com/cpp-pm/gate/v0.10.0/cmake/HunterGate.cmake
     ${INTX_DEPS_DOWNLOAD_DIR}/HunterGate.cmake
-    EXPECTED_HASH SHA256=08d57ca550856c2f647c4863a25fa386e0d30e905e096a8eaeec336513b14ed1
+    EXPECTED_HASH SHA256=3da4dcd5dca35679d6c822c0ac23b7b91d86d481f838bad4baca88410b4bde72
 )
 include(${INTX_DEPS_DOWNLOAD_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL "https://github.com/cpp-pm/hunter/archive/v0.24.0.tar.gz"
-    SHA1 "a3d7f4372b1dcd52faa6ff4a3bd5358e1d0e5efd"
-    LOCAL
+    URL "https://github.com/cpp-pm/hunter/archive/v0.25.3.tar.gz"
+    SHA1 "0dfbc2cb5c4cf7e83533733bdfd2125ff96680cb"
 )
