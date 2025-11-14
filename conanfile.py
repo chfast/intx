260d636f4367926607f122f200ac06f4e8e11cc4
Paweł Bylica <pawel@hepcolgum.band>
2025-11-14 10:51:35 +0100

    Add support for Conan package manager (#352)

---
+++ conanfile.py
@@ -0,0 +1,24 @@
+from conan import ConanFile
+from conan.tools.files import copy
+
+
+class IntxConan(ConanFile):
+    name = "intx"
+    version = "0.13.0"
+    description = "Extended precision integer C++ library"
+    url = "https://github.com/intx/intx"
+    license = "Apache-2.0"
+    topics = ("integer", "arbitrary-precision", "extended-precision", "biginteger",
+              "uint128", "uint256", "uint512")
+
+    package_type = "header-library"
+    exports_sources = "include/*"
+    no_copy_source = True
+    test_package_folder = "test/integration/conan_package"
+
+    def package(self):
+        copy(self, "*", self.source_folder, self.package_folder)
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
